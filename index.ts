f2155a07441e8136011b4169ba8c7dd9eac769ae
tomfuertes <tomfuertes@gmail.com>
2025-06-06 20:32:26 -0500

    build and test random dice roll

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+import { input } from "@inquirer/prompts";
+
 console.log("Hello via Bun!");
+
+// prompt for a string of random dice
+const digits = await input({
+  message: "Enter a string of random digits",
+});
+
+// convert the string to an array of numbers
+const numbers = digits?.split("").map(Number) || [];
+
+// print the numbers
+console.log(numbers);
+
+// print the sum of the numbers
+console.log(numbers.reduce((a, b) => a + b, 0));
